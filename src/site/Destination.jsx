e16266dbd9f446bc3f60079102517aca8cf4e76b
Mateusz Czernik <m.czernik12@gmail.com>
2023-07-27 18:38:34 +0200

    destinations styles and mapping data

--- src/site/Destination.jsx
+++ src/site/Destination.jsx
@@ -1,34 +1,41 @@
 import "../scss/destination.scss";
-// import { useState } from "react";
+import { useState } from "react";
 import { Header } from "../components/Header";
-// import Data from "../json/data.json"
+import Data from "../json/data.json"
 export const Destination = () => {
-  // const [activePlanet, srtActivePlanet] = useState(1)
+  const [activePlanet, setActivePlanet] = useState(1)
 
   return (
     <>
       <Header/>
       <main className="dest-main-cont">
         <p className="dest-title"><span className="title-number">01</span> PICK YOUR DESTINATION</p>
-        <section className="planets-wrapper">
-          <div className="planet-img-wrapper">
-            <img src="public/destination/image-moon.png" alt="planet image" className="planet-img"/>
-          </div>
-          <div className="planet-info-wrapper">
-            <ul className="planets-list">
-              <li className="planet-name">MOON</li>
-              <li className="planet-name">MARS</li>
-              <li className="planet-name">EUROPA</li>
-              <li className="planet-name">TITAN</li>
-            </ul>
-            <h1 className="planet-title">MOON</h1>
-            <p className="planet-desc">See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.</p>
-            <div className="additional-info">
-              <p className="distance-info">384,400 km</p>
-              <p className="travel-info">3 days</p>
-            </div>
-          </div>
-        </section>
+        {Data.destinations.filter(item => item.id === activePlanet).map(item => {
+          return (
+            <>
+              <section className="planets-wrapper">
+                <div className="planet-img-wrapper">
+                  <img src={item.images.png} alt="planet image" className="planet-img"/>
+                </div>
+                <div className="planet-info-wrapper">
+                  <ul className="planets-list">
+                    {Data.destinations.map((item) => {
+                      return (
+                        <li key={item.id} className="planet-name" data-active={item.id === activePlanet ? "true" : "flase"} onClick={() => setActivePlanet(item.id)}>{item.name}</li>
+                      )
+                    })}
+                  </ul>
+                  <h1 className="planet-title">{item.name}</h1>
+                  <p className="planet-desc">{item.description}</p>
+                  <div className="additional-info">
+                    <p className="distance-info">{item.distance}</p>
+                    <p className="travel-info">{item.travel}</p>
+                  </div>
+                </div>
+              </section>
+            </>
+          )
+        })}
       </main>
     </>
     
