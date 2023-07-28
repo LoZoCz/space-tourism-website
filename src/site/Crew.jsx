326bb20e0bcdc0ce26e8a62d807021315b934bee
Mateusz Czernik <m.czernik12@gmail.com>
2023-07-28 13:03:33 +0200

    tech site done

--- src/site/Crew.jsx
+++ src/site/Crew.jsx
@@ -1,17 +1,58 @@
-import "../scss/crew.scss";
-import { Header } from "../components/Header";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircle } from '@fortawesome/free-solid-svg-icons'
-import { useState } from "react";
-import Data from '../json/data.json'
+import { Header } from '../components/Header';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
+import Data from '../json/data.json';
+import { useState, useEffect, useCallback } from 'react';
+import "../scss/crew.scss"
+
 export const Crew = () => {
   const [activeMember, setActiveMember] = useState(1);
+  const [isAutoRotate, setAutoRotate] = useState(true);
+  const [isPageVisible, setPageVisible] = useState(true);
 
-  return (<>
-    <Header />
-    <main className="crew-main-cont">
-        <p className="crew-title"><span className="title-number">02</span> Meet your crew</p>
-        {Data.crew.filter(item => item.id === activeMember).map(item => {
+  // Funkcja do przesuwania wskaźnika aktywnej osoby co 5 sekund
+  const rotateMembers = useCallback(() => {
+    if (isAutoRotate) {
+      setActiveMember((prevMember) => (prevMember % Data.crew.length) + 1);
+    }
+  }, [isAutoRotate]);
+
+  // Obsługa zdarzenia visibilitychange
+  const handleVisibilityChange = () => {
+    setPageVisible(!document.hidden);
+  };
+
+  useEffect(() => {
+    // Uruchamiamy setInterval po zamontowaniu komponentu
+    const intervalId = setInterval(rotateMembers, 8000);
+
+    // Obsługa zdarzenia visibilitychange i blur
+    document.addEventListener('visibilitychange', handleVisibilityChange);
+    window.addEventListener('blur', handleVisibilityChange);
+
+    // Czyszczenie interwału i usuwanie listenerów po odmontowaniu komponentu
+    return () => {
+      clearInterval(intervalId);
+      document.removeEventListener('visibilitychange', handleVisibilityChange);
+      window.removeEventListener('blur', handleVisibilityChange);
+    };
+  }, [isAutoRotate, isPageVisible, rotateMembers]);
+
+  // Obsługa zmiany osoby po kliknięciu na wskaźnik
+  const handleIndicatorClick = (id) => {
+    setActiveMember(id);
+    setAutoRotate(false); // Zatrzymujemy zmianę automatyczną na 4 sekundy
+    setTimeout(() => setAutoRotate(true), 4000); // Wznawiamy zmianę automatyczną po 4 sekundach
+  };
+
+  return (
+    <>
+      <Header />
+      <main className="crew-main-cont">
+        <p className="crew-title">
+          <span className="title-number">02</span> Meet your crew
+        </p>
+        {Data.crew.filter((item) => item.id === activeMember).map((item) => {
           return (
             <section className="crew-wrapper" key={item.id}>
               <div className="crew-info-box">
@@ -21,21 +62,31 @@ export const Crew = () => {
                   <p className="crew-desc">{item.bio}</p>
                 </div>
                 <ul className="indicator-list">
-                  {Data.crew.map(item => {
+                  {Data.crew.map((item) => {
                     return (
-                    <li className="indicator" data-active={item.id === activeMember ? "true" : "flase"} key={item.id} onClick={() => setActiveMember(item.id)}>
-                      <FontAwesomeIcon icon={faCircle} />
-                    </li>
-                  )
+                      <li
+                        className="indicator"
+                        data-active={item.id === activeMember ? 'true' : 'false'}
+                        key={item.id}
+                        onClick={() => handleIndicatorClick(item.id)}
+                      >
+                        <FontAwesomeIcon icon={faCircle} />
+                      </li>
+                    );
                   })}
                 </ul>
               </div>
               <div className="crew-image-box">
-                <img src={item.images.png} alt="crew member" className="crew-image"/>
+                <img
+                  src={item.images.webp}
+                  alt="crew member"
+                  className="crew-image"
+                />
               </div>
             </section>
-          )
+          );
         })}
-    </main>  
-  </>);
+      </main>
+    </>
+  );
 };
--- src/site/Technology.jsx
+++ src/site/Technology.jsx
@@ -1,10 +1,42 @@
 import "../scss/technology.scss";
 import { Header } from "../components/Header";
+import { useState } from "react";
+import Data from '../json/data.json'
 export const Technology = () => {
+  const [activeTech, setActiveTech] = useState(1)
+
   return (
     <>
-    <Header/>
-    <h1>Technology</h1>
+      <Header/>
+      <main className="tech-main-cont">
+        <p className="tech-title">
+          <span className="title-number">03</span> Space lunch 101
+        </p>
+        {Data.technology.filter(item => item.id === activeTech).map(item => {
+          return (
+            <section className="tech-wrapper" key={item.id}>
+              <ul className="indicator-list">
+                {Data.technology.map((item, index) => {
+                  return (
+                  <li className="indicator" key={item.id} data-active={item.id === activeTech ? 'true' : 'false'} onClick={() => setActiveTech(item.id)}>
+                    {index + 1}
+                  </li>)
+                })}
+              </ul>
+              <div className="tech-info-box">
+                <div className="text-box">
+                  <p className="tech-eyebrow">The terminology...</p>
+                  <h1 className="tech-title">{item.name}</h1>
+                  <p className="tech-desc">{item.description}</p>
+                </div>
+              </div>
+              <div className="image-wrapper">
+                <img src={item.images.portrait} alt="technology image" />
+              </div>
+            </section>
+          )
+        })}
+      </main>
     </>
   );
 };
